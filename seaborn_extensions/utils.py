999959a7b2f67128ea0c7ca7686400e575cb79c8
afrendeiro <afrendeiro@gmail.com>
2021-12-13 15:30:45 -0500

    allow unlimited number of categories in clustermap row,col_colors

--- seaborn_extensions/utils.py
+++ seaborn_extensions/utils.py
@@ -191,7 +191,55 @@ def get_categorical_cmap(x: Series) -> matplotlib.colors.ListedColormap:
             )[:n],
             name=f"tab40-{n}",
         )
-    raise ValueError("Only up to 40 unique values can be plotted as color.")
+    # raise ValueError("Only up to 40 unique values can be plotted as color.")
+    return matplotlib.colors.ListedColormap(colors=get_n_colors(n), name=f"random-{n}")
+
+
+def get_n_colors(n: int, max_value: float = 1.0) -> Array:
+    """
+    With modifications from https://stackoverflow.com/a/13781114/1469535
+    """
+    import itertools
+    from fractions import Fraction
+    import colorsys
+
+    def zenos_dichotomy():
+        """
+        http://en.wikipedia.org/wiki/1/2_%2B_1/4_%2B_1/8_%2B_1/16_%2B_%C2%B7_%C2%B7_%C2%B7
+        """
+        for k in itertools.count():
+            yield Fraction(1, 2 ** k)
+
+    def fracs():
+        """
+        [Fraction(0, 1), Fraction(1, 2), Fraction(1, 4), Fraction(3, 4), Fraction(1, 8), Fraction(3, 8), Fraction(5, 8), Fraction(7, 8), Fraction(1, 16), Fraction(3, 16), ...]
+        [0.0, 0.5, 0.25, 0.75, 0.125, 0.375, 0.625, 0.875, 0.0625, 0.1875, ...]
+        """
+        yield Fraction(0)
+        for k in zenos_dichotomy():
+            i = k.denominator  # [1,2,4,8,16,...]
+            for j in range(1, i, 2):
+                yield Fraction(j, i)
+
+    # can be used for the v in hsv to map linear values 0..1 to something that looks equidistant
+    # bias = lambda x: (math.sqrt(x/3)/Fraction(2,3)+Fraction(1,3))/Fraction(6,5)
+    def hue_to_tones(h):
+        for s in [Fraction(6, 10)]:  # optionally use range
+            for v in [Fraction(8, 10), Fraction(5, 10)]:  # could use range too
+                yield (h, s, v)  # use bias for v here if you use range
+
+    def hsv_to_rgb(x):
+        return colorsys.hsv_to_rgb(*map(float, x))
+
+    flatten = itertools.chain.from_iterable
+
+    def hsvs():
+        return flatten(map(hue_to_tones, fracs()))
+
+    def rgbs():
+        return map(hsv_to_rgb, hsvs())
+
+    return np.asarray(list(itertools.islice(rgbs(), n))) * max_value
 
 
 def to_color_series(x: Series, cmap: tp.Optional[str] = None) -> Series:
